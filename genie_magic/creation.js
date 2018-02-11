77a1c402fe7847a28d9f31c2bb2c725218e3909a
S.Weatherall <sweather131@gmail.com>
2018-02-11 16:30:17 -0500

    implement fxn hoisting

--- genie_magic/creation.js
+++ genie_magic/creation.js
@@ -2,64 +2,15 @@ var fs = require('fs-extra');
 var path = require('path');
 var creationTemplate = require('./creation_template.js');
 
-// paths relative to `package.json`
+// all paths are relative to `package.json`
 var grantableWishesPath = './grantable_wishes';
 var grantableWishesMetaPath = './grantable_wishes__meta';
 var wishAssets = ['./wish_assets/imgs', './wish_assets/css', './wish_assets/js'];
 var wishGrantedPath = './wish_granted';
-
 // a place to store wishes + wish metadata
 var grantableWishes = {}, grantableWishesMeta = {};
 
 console.log('template genie summoned...');
-
-// clean up the previously granted wish
-var expelOldWish = function(files) {
-  for (var file of files) {
-    fs.removeSync(path.join(wishGrantedPath, file));
-  }
-};
-
-// read everything from grantable_wishes directory
-var readWishes = function() {
-  console.log('realizing grantable wishes...');
-  for(var wish of fs.readdirSync(grantableWishesPath)){
-    grantableWishes[wish] = fs.readFileSync(path.join(grantableWishesPath,wish),'utf8');
-  }
-};
-// read everything from grantable_wishes_meta directory
-var readWishesMeta = function() {
-  console.log('realizing grantable wishes meta...');
-  for(var wishMeta of fs.readdirSync(grantableWishesMetaPath)){
-    grantableWishesMeta[wishMeta] = fs.readFileSync(path.join(grantableWishesMetaPath,wishMeta),'utf8');
-  }
-};
-
-// grant each wish from the data provided, using the markdown template
-var grantWishes = function() {
-  console.log('granting wishes...');
-  for(var wish of Object.entries(grantableWishes)) {
-    var pageName = wish[0].slice(0, wish[0].lastIndexOf('.'));
-    var metaData = grantableWishesMeta.hasOwnProperty(pageName+'.json')
-      ? JSON.parse(grantableWishesMeta[pageName+'.json'])
-      : {};
-    metaData.title = metaData.title || pageName;
-    var pageContent = wish[1];
-    fs.writeFileSync(
-      path.join(wishGrantedPath,pageName+'.html'),
-      creationTemplate.createPage(pageContent, metaData));
-  }
-}
-
-// copy directories + assets into wish_granted folder
-var revealWishes = function() {
-  console.log('presenting wishes...');
-  for(var asset of wishAssets){
-    fs.copySync(asset, path.join(wishGrantedPath,asset));
-  }
-}
-
-
 
 fs.readdir(wishGrantedPath)
 .then(expelOldWish, function(err) {
@@ -95,3 +46,50 @@ fs.readdir(wishGrantedPath)
 }).then(function() {
   console.log('fin!');
 });
+
+
+// clean up the previously granted wish
+function expelOldWish(files) {
+  for (var file of files) {
+    fs.removeSync(path.join(wishGrantedPath, file));
+  }
+};
+
+// read everything from grantable_wishes directory
+function readWishes() {
+  console.log('realizing grantable wishes...');
+  for(var wish of fs.readdirSync(grantableWishesPath)){
+    grantableWishes[wish] = fs.readFileSync(path.join(grantableWishesPath,wish),'utf8');
+  }
+};
+// read everything from grantable_wishes_meta directory
+function readWishesMeta() {
+  console.log('realizing grantable wishes meta...');
+  for(var wishMeta of fs.readdirSync(grantableWishesMetaPath)){
+    grantableWishesMeta[wishMeta] = fs.readFileSync(path.join(grantableWishesMetaPath,wishMeta),'utf8');
+  }
+};
+
+// grant each wish from the data provided, using the markdown template
+function grantWishes() {
+  console.log('granting wishes...');
+  for(var wish of Object.entries(grantableWishes)) {
+    var pageName = wish[0].slice(0, wish[0].lastIndexOf('.'));
+    var metaData = grantableWishesMeta.hasOwnProperty(pageName+'.json')
+      ? JSON.parse(grantableWishesMeta[pageName+'.json'])
+      : {};
+    metaData.title = metaData.title || pageName;
+    var pageContent = wish[1];
+    fs.writeFileSync(
+      path.join(wishGrantedPath,pageName+'.html'),
+      creationTemplate.createPage(pageContent, metaData));
+  }
+}
+
+// copy directories + assets into wish_granted folder
+function revealWishes() {
+  console.log('presenting wishes...');
+  for(var asset of wishAssets){
+    fs.copySync(asset, path.join(wishGrantedPath,asset));
+  }
+}
